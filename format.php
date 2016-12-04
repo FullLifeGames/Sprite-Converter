bf0f6827663d9cafd1e100e46ac193caa2dc7847
FullLifeGames <FullLifeGames@users.noreply.github.com>
2016-12-04 14:05:35 +0100

    Gen 7 Update

--- format.php
+++ format.php
@@ -42,18 +42,19 @@
 			$json = json_decode($json,true);
 			$nameToDex = array();
 			foreach($json as $key => $value){
-				$nameToDex[trim($value["name"])] = $value["dex"];
-				$nameToDex[trim($value["name"])."-Mega"] = $value["dex"]."-m";
-				$nameToDex[trim($value["name"])."-Mega-X"] = $value["dex"]."-mx";
-				$nameToDex[trim($value["name"])."-Mega-Y"] = $value["dex"]."-my";
-				$nameToDex[trim($value["name"])."-Therian"] = $value["dex"]."-s";
+				if(!isset($nameToDex[trim($value["name"])])) $nameToDex[trim($value["name"])] = $value["dex"];
+				if(!isset($nameToDex[trim($value["name"])."-Alola"])) $nameToDex[trim($value["name"])."-Alola"] = $value["dex"]."-a";
+				if(!isset($nameToDex[trim($value["name"])."-Mega"])) $nameToDex[trim($value["name"])."-Mega"] = $value["dex"]."-m";
+				if(!isset($nameToDex[trim($value["name"])."-Mega-X"])) $nameToDex[trim($value["name"])."-Mega-X"] = $value["dex"]."-mx";
+				if(!isset($nameToDex[trim($value["name"])."-Mega-Y"])) $nameToDex[trim($value["name"])."-Mega-Y"] = $value["dex"]."-my";
+				if(!isset($nameToDex[trim($value["name"])."-Therian"])) $nameToDex[trim($value["name"])."-Therian"] = $value["dex"]."-s";
 			}
 		}
 		$list = array();
 		echo "Links: <br>";
 		$import = preg_split('/\r\n|[\r\n]/',  $_POST["team"]);
 		foreach($import as $key => $value){
-			if(strpos($value, "@")!==false || (isset($import[$key+1]) && strpos($import[$key+1], "Ability:") !== false)){
+			if((strpos($value, "@")!==false || (isset($import[$key+1]) && strpos($import[$key+1], "Ability:") !== false) || (isset($import[$key-1]) && trim($import[$key-1]) === "") || (!isset($import[$key-1]))) && trim($value) !== ""){
 				$name = "";
 				if(strpos($value, "(M)")!==false || strpos($value, "(F)")!==false){
 					$value = str_replace("(M)", "" , $value);
@@ -77,22 +78,22 @@
 				}
 				if(strpos($_POST["tier"], "sprites")!==false){
 					$name = trim($name);
-					if((strpos($name, "Arceus")!==false || strpos($name, "Basculin")!==false || strpos($name, "Gourgeist")!==false || strpos($name, "Pumpkaboo")!==false || strpos($name, "Pikachu")!==false) && strpos($name, "-")!==false){
+					if((strpos($name, "Arceus")!==false || strpos($name, "Basculin")!==false || strpos($name, "Gourgeist")!==false || strpos($name, "Pumpkaboo")!==false || strpos($name, "Pikachu")!==false) && strpos($name, "-")!==false && strpos($name, "-Alola") === false){
 						$name = substr($name, 0, strpos($name, "-"));
 					}
 					if(!isset($nameToDex[$name]) && strpos($name, "-")!==false && (strpos($name, "Rotom")!==false || strpos($name, "Deoxys")!==false || strpos($name, "Giratina")!==false)){
 						$ext = strtolower(substr($name, strpos($name, "-") + 1, 1));
 						$name = substr($name, 0, strpos($name, "-"));
-						echo "http://www.serebii.net/pokedex-xy/icon/" . $nameToDex[$name] . $ext . ".png<br>";
-						$list[] = "http://www.serebii.net/pokedex-xy/icon/" . $nameToDex[$name] . $ext . ".png";
+						echo "http://www.serebii.net/pokedex-sm/icon/" . $nameToDex[$name] . $ext . ".png<br>";
+						$list[] = "http://www.serebii.net/pokedex-sm/icon/" . $nameToDex[$name] . $ext . ".png";
 					} else if(!isset($nameToDex[$name]) && strpos($name, "-")!==false){
 						$ext = strtolower(substr($name, strpos($name, "-"), 2));
 						$name = substr($name, 0, strpos($name, "-"));
-						echo "http://www.serebii.net/pokedex-xy/icon/" . $nameToDex[$name] . $ext . ".png<br>";
-						$list[] = "http://www.serebii.net/pokedex-xy/icon/" . $nameToDex[$name] . $ext . ".png";
+						echo "http://www.serebii.net/pokedex-sm/icon/" . $nameToDex[$name] . $ext . ".png<br>";
+						$list[] = "http://www.serebii.net/pokedex-sm/icon/" . $nameToDex[$name] . $ext . ".png";
 					} else {
-						echo "http://www.serebii.net/pokedex-xy/icon/" . $nameToDex[$name] . ".png<br>";
-						$list[] = "http://www.serebii.net/pokedex-xy/icon/" . $nameToDex[$name] . ".png";
+						echo "http://www.serebii.net/pokedex-sm/icon/" . $nameToDex[$name] . ".png<br>";
+						$list[] = "http://www.serebii.net/pokedex-sm/icon/" . $nameToDex[$name] . ".png";
 					}
 				} else {
 					echo "http://play.pokemonshowdown.com/sprites/". htmlentities($_POST["tier"]) . "/".htmlentities($name).$ext."<br>";
