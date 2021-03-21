5612ccf3aade26223c110c8c177fc0c91a5fbfee
FullLifeGames <FullLifeGames@users.noreply.github.com>
2021-03-21 18:56:34 +0100

    Update and rename format.php to index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Sprite Converter</title>
-        <link rel="stylesheet" href="/css/bootstrap.min.css">
-        <script src="/js/jquery.min.js"></script>
-        <script src="/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+	<script src="/js/jquery.min.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<div class="container">
